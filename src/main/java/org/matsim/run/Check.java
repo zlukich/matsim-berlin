6d0c68d59b11abb0bdb9bf65d562bb050e5b4f88
hacas <hacas@DESKTOP-E1PQH7N>
2021-01-07 13:47:19 +0100

    Prapating for computing

---
+++ src/main/java/org/matsim/run/Check.java
@@ -0,0 +1,11 @@
+package org.matsim.run;
+
+public class Check {
+	
+	 public static void main(String[] args) {
+	        System.out.println(
+	            com.sun.media.imageioimpl.common.PackageUtil.getVendor()
+	        );
+	    }
+
+}
